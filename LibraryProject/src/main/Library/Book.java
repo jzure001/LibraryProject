775a91a7ca0d2fd75e5637d2c37b2e0acb1237da
Jzure001 <jzurek359@gmail.com>
2023-04-10 15:16:47 -0400

    FirstCommit JZ

---
+++ LibraryProject/src/main/Library/Book.java
@@ -0,0 +1,28 @@
+public class Book {
+    private String id;
+    private String description;
+    private boolean isRented;
+
+    public Book(String id, String description){
+        this.id = id;
+        this.description=description;
+        this.isRented=false;
+    }
+
+    public String getId(){
+        return id;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public boolean isRented(){
+        return isRented;
+    }
+
+    public void setRented{
+        this.isRented=isRented;
+    }
+
+}
---
+++ LibraryProject/src/main/Library/Library.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+
+public class Library {
+    private HashMap<Integer, LibraryUser> users;
+    private HashMap<String, Book> books;
+
+}
