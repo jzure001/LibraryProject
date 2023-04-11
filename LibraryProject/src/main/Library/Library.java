f9687fa40b0a9c71b09f875631d6af8e7a761a39
Jzure001 <jzurek359@gmail.com>
2023-04-11 12:56:33 -0400

    Updated Library Class

--- LibraryProject/src/main/Library/Library.java
+++ LibraryProject/src/main/Library/Library.java
@@ -5,4 +5,30 @@ public class Library {
     private HashMap<Integer, LibraryUser> users;
     private HashMap<String, Book> books;
 
+    public Library(){
+        users= new HashMap<Integer, LibraryUser>();
+        books = new HashMap<String, Book>();
+
+    }
+
+    public void registerUser(int id){
+        users.put(id, new LibraryUser());
+    }
+
+    public boolean isUserRegistered(int id){
+        return users.containsKey(id);
+    }
+
+    public void addBook(String id, String description){
+        books.put(id, new Book(id, description));
+    }
+
+    public Book searchBook(String id){
+        return books.get(id);
+    }
+
+    public void removeBook(String id){
+        books.remove(id);
+    }
+
 }
